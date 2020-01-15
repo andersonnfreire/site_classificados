1a115b318574f8c7e9887c175875eb1dc8ce86a6
Anderson N Freire <coolerinformatica1995@gmail.com>
2020-01-15 20:40:36 -0200

    Criando o layout do classificados

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Classificados</title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php require './pages/header/header.php'; ?>
+
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h2>Nós temos hoje 999 anúncios</h2>
+        <p>E mais de 999 usuários cadastrados</p>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-3">
+            <h4>Pesquisa Avançada</h4>
+        </div>
+        <div class="col-sm-9">
+            <h3>Ultimos anuncios</h3>
+        </div>
+
+    </div>
+</div>
+
+<?php require './pages/footer/footer.php';?>
---
+++ pages/header/header.php
@@ -0,0 +1,40 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Classificados</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="assets/css/style.css"/>
+        <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js" rel="stylesheet"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js" rel="stylesheet"></script>
+        <script type="text/javascript" src="assets/js/script.js" rel="stylesheet"></script>
+    </head>
+    <body>
+        
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./">Classificados</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cadastra-se</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+<!--                        <li class="nav-item">
+                            <a class="nav-link" href="#">Meus Anúncios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sair</a>
+                        </li>-->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
